f43a781a24e98a6ff877cd3f2f3f237d309da91a
Rouclec <senatorasonganyi97@gmail.com>
2024-03-31 23:36:03 +0100

    not loading variables from config yet

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -8,21 +8,22 @@ import (
 
 	"github.com/jackc/pgx/v5/pgxpool"
 	_ "github.com/jackc/pgx/v5/stdlib"
-	"github.com/rouclec/simplebank/util"
 )
 
 var testQueries *Queries
 var pool *pgxpool.Pool
 
+const dbSource = "postgresql://postgres:changemeinprod%21@localhost:5432/simple_bank?sslmode=disable"
+
 func TestMain(m *testing.M) {
 	var err error
 
-	config, err := util.LoadConfig("../..")
-	if err != nil {
-		log.Fatal("Error parsing database config: ", err)
-	}
+	// config, err := util.LoadConfig("../..")
+	// if err != nil {
+	// 	log.Fatal("Error parsing database config: ", err)
+	// }
 
-	pool, err = pgxpool.New(context.Background(), config.DBSource)
+	pool, err = pgxpool.New(context.Background(), dbSource)
 	if err != nil {
 		log.Fatal("Error connecting to database: ", err)
 	}
